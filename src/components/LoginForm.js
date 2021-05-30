cd93f7f55aa43f66b0a48eb8d03d1f82f59d3c74
Nishit2011 <nranjan.2004@gmail.com>
2021-05-30 18:39:45 +0530

    ErrorMessage, Form, Field(Formik Components) implemented

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button, Container, Form } from "react-bootstrap";
-import { useFormik } from "formik";
+
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const LoginForm = () => {
@@ -8,84 +8,67 @@ const LoginForm = () => {
     email: "",
     password: "",
   };
-  const validate = (values) => {
-    let errors = {};
+  // const validate = (values) => {
+  //   let errors = {};
 
-    if (!values.email) {
-      errors.email = "Required";
-    }
-    if (!values.password) {
-      errors.password = "Requred";
-    }
-    return errors;
-  };
+  //   if (!values.email) {
+  //     errors.email = "Required";
+  //   }
+  //   if (!values.password) {
+  //     errors.password = "Requred";
+  //   }
+  //   return errors;
+  // };
 
   const onSubmit = (values) => {
     console.log("--", values);
   };
   const validationSchema = Yup.object({
-    name: Yup.string().required("Required!!!"),
+    password: Yup.string().required("Required!!!"),
     email: Yup.string().email("Invalid format").required("Required!!!"),
   });
 
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema,
-  });
+  // const formik = useFormik({
+  //   initialValues,
+  //   onSubmit,
+  //   validationSchema,
+  // });
 
-  console.log("Visited fieldds", formik.touched);
+  // console.log("Visited fieldds", formik.touched);
   //console.log(formik.values);
 
   return (
     <div>
-      <Container>
-        <Form onSubmit={formik.handleSubmit}>
-          <Form.Group className="mb-2" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            {formik.errors.email && formik.touched.email ? (
-              <div>{formik.errors.email}</div>
-            ) : (
-              <></>
-            )}
-            <Form.Control
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <Form>
+          <div className="form-control">
+            <label htmlFor="email">Email</label>
+            <Field
               type="email"
-              placeholder="Enter email"
-              className="mb-2"
+              id="email"
               name="email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
+              placeholder="Enter email"
             />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+            <ErrorMessage name="email" />
+          </div>
 
-          <Form.Group className="mb-2" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            {formik.errors.password && formik.touched.password ? (
-              <div>{formik.errors.password}</div>
-            ) : (
-              <></>
-            )}
-            <Form.Control
+          <div className="form-control">
+            <label htmlFor="password">Password</label>
+            <Field
               type="password"
-              placeholder="Password"
+              id="password"
               name="password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
+              placeholder="Enter password"
             />
-          </Form.Group>
-          <Form.Group className="mb-2" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
+            <ErrorMessage name="password" />
+          </div>
+          <button type="submit">Submit</button>
         </Form>
-      </Container>
+      </Formik>
     </div>
   );
 };
