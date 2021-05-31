88e8ff0d9bd13b4883eeab3e6bfb12d1c6e8edb8
Nishit2011 <nranjan.2004@gmail.com>
2021-05-31 14:18:00 +0530

    Loading Data on Page Load, instead of initial values, and disabling suubmit button till all validations are checked.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
 import * as Yup from "yup";
 
 const LoginForm = (props) => {
+  const [formValues, setFormValues] = useState(null);
+
   const initialValues = {
     email: "",
     password: "",
@@ -28,6 +30,19 @@ const LoginForm = (props) => {
   //   return errors;
   // };
 
+  const savedValues = {
+    email: "nishit@gmail.com",
+    password: "123456",
+    address: "Ranchi",
+    country: "India",
+    city: {
+      pincode: "834002",
+      state: "JH",
+    },
+    phNum: ["88888800", "78787878"],
+    phoneNumbers: ["123455678"],
+  };
+
   const validateAddress = (value) => {
     let err;
     if (!value) {
@@ -44,8 +59,8 @@ const LoginForm = (props) => {
     email: Yup.string().email("Invalid format").required("Required!!!"),
     // address: Yup.string().required("Req!!!"),
     country: Yup.string().required("Req!!!!"),
-    pincode: Yup.string().required("Pin req!!"),
-    state: Yup.string().required("State required!!!!!"),
+    // pincode: Yup.string().required("Pin req!!"),
+    // state: Yup.string().required("State required!!!!!"),
   });
 
   // const formik = useFormik({
@@ -57,131 +72,147 @@ const LoginForm = (props) => {
   // console.log("Visited fieldds", formik.touched);
   //console.log(formik.values);
 
+  useEffect(() => {
+    setFormValues(savedValues);
+  }, []);
+
   return (
     <div>
       <Formik
-        initialValues={initialValues}
+        initialValues={savedValues}
         validationSchema={validationSchema}
         onSubmit={onSubmit}
+        validateOnMount
+        enableReinitialize
       >
-        <Form>
-          <div className="form-control">
-            <label htmlFor="email">Email</label>
-            <Field
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter email"
-            />
-            <ErrorMessage name="email" />
-          </div>
+        {(formik) => {
+          console.log("formik props:--", formik);
+          return (
+            <Form>
+              <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <Field
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Enter email"
+                />
+                <ErrorMessage name="email" />
+              </div>
 
-          <div className="form-control">
-            <label htmlFor="password">Password</label>
-            <Field
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Enter password"
-            />
-            <ErrorMessage name="password" />
-          </div>
+              <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <Field
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Enter password"
+                />
+                <ErrorMessage name="password" />
+              </div>
 
-          <div className="form-control">
-            <label htmlFor="address">Address</label>
-            <Field
-              as="textarea"
-              id="address"
-              name="address"
-              placeholder="Enter address"
-              validate={validateAddress}
-            />
-            <ErrorMessage name="address" />
-          </div>
+              <div className="form-control">
+                <label htmlFor="address">Address</label>
+                <Field
+                  as="textarea"
+                  id="address"
+                  name="address"
+                  placeholder="Enter address"
+                  validate={validateAddress}
+                />
+                <ErrorMessage name="address" />
+              </div>
 
-          <div className="form-control">
-            <label htmlFor="pincode"></label>
-            <Field
-              id="pincode"
-              name="city.pincode"
-              placeholder="Enter Pincode"
-            ></Field>
-          </div>
-          <ErrorMessage name="city.pincode" />
-          <div className="form-control">
-            <label htmlFor="state"></label>
-            <Field
-              id="state"
-              name="city.state"
-              placeholder="Enter state"
-            ></Field>
-            <ErrorMessage name="city.state" />
-          </div>
+              {/* <div className="form-control">
+                <label htmlFor="pincode"></label>
+                <Field
+                  id="pincode"
+                  name="city.pincode"
+                  placeholder="Enter Pincode"
+                ></Field>
+              </div>
+              <ErrorMessage name="city.pincode" />
+              <div className="form-control">
+                <label htmlFor="state"></label>
+                <Field
+                  id="state"
+                  name="city.state"
+                  placeholder="Enter state"
+                ></Field>
+                <ErrorMessage name="city.state" />
+              </div> */}
 
-          <div className="form-control">
-            <label htmlFor="country">Country</label>
-            <Field name="country">
-              {(props) => {
-                console.log(props);
-                const { field, form, meta } = props;
-                return <input id="country" {...field} />;
-              }}
-            </Field>
+              <div className="form-control">
+                <label htmlFor="country">Country</label>
+                <Field name="country">
+                  {(props) => {
+                    console.log(props);
+                    const { field, form, meta } = props;
+                    return <input id="country" {...field} />;
+                  }}
+                </Field>
 
-            <ErrorMessage name="country" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="primary-phn"></label>
-            <Field
-              name="phNum[0]"
-              type="text"
-              placeholder="Enter primary phone"
-            ></Field>
+                <ErrorMessage name="country" />
+              </div>
+              <div className="form-control">
+                <label htmlFor="primary-phn"></label>
+                <Field
+                  name="phNum[0]"
+                  type="text"
+                  placeholder="Enter primary phone"
+                ></Field>
 
-            <ErrorMessage name="phNum[0]" />
-          </div>
+                <ErrorMessage name="phNum[0]" />
+              </div>
 
-          <div className="form-control">
-            <label htmlFor="secondary-phn"></label>
-            <Field
-              name="phNum[1]"
-              type="text"
-              placeholder="Enter secondary phone"
-            ></Field>
+              <div className="form-control">
+                <label htmlFor="secondary-phn"></label>
+                <Field
+                  name="phNum[1]"
+                  type="text"
+                  placeholder="Enter secondary phone"
+                ></Field>
 
-            <ErrorMessage name="phNum[1]" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="list-of-phnNumbers"></label>
-            <FieldArray name="phoneNumbers">
-              {(fieldArrayProps) => {
-                // console.log(fieldArrayProps);
-                const { push, remove, form } = fieldArrayProps;
-                const { values } = form;
-                const { phoneNumbers } = values;
-                return (
-                  <div>
-                    {phoneNumbers.map((phoneNumber, index) => (
-                      <div key={index}>
-                        <Field name={`phoneNumbers[${index}]`} />
-                        <button type="button" onClick={() => remove(index)}>
-                          -
-                        </button>
-                        <button type="button" onClick={() => push("")}>
-                          +
-                        </button>
+                <ErrorMessage name="phNum[1]" />
+              </div>
+              <div className="form-control">
+                <label htmlFor="list-of-phnNumbers"></label>
+                <FieldArray name="phoneNumbers">
+                  {(fieldArrayProps) => {
+                    // console.log(fieldArrayProps);
+                    const { push, remove, form } = fieldArrayProps;
+                    const { values } = form;
+                    const { phoneNumbers } = values;
+                    return (
+                      <div>
+                        {phoneNumbers.map((phoneNumber, index) => (
+                          <div key={index}>
+                            <Field name={`phoneNumbers[${index}]`} />
+                            <button type="button" onClick={() => remove(index)}>
+                              -
+                            </button>
+                            <button type="button" onClick={() => push("")}>
+                              +
+                            </button>
+                          </div>
+                        ))}
                       </div>
-                    ))}
-                  </div>
-                );
-              }}
-            </FieldArray>
-          </div>
+                    );
+                  }}
+                </FieldArray>
+              </div>
 
-          <div className="form-control">
-            <button type="submit">Submit</button>
-          </div>
-        </Form>
+              <div className="form-control">
+                <button
+                  type="submit"
+                  disabled={!(formik.dirty && formik.isValid)}
+                >
+                  Submit
+                </button>
+              </div>
+            </Form>
+          );
+        }}
       </Formik>
     </div>
   );
